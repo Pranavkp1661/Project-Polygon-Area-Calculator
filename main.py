b402c23692df5b9f24162713d404e54de55186f0
pranavkp1661 <pranavp7645357@gmail.com>
2021-09-18 13:45:42 +0530

    code finish

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import shape_calculator
+from unittest import main
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+rect = shape_calculator.Rectangle(5, 10)
+print(rect.get_area())
+rect.set_width(3)
+print(rect.get_perimeter())
+print(rect)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+sq = shape_calculator.Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
